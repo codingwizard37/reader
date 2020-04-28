75ad30fdbab9c51b96d39d814e52a161014b0832
Jake Searle <jacobbsearle@gmail.com>
2020-04-27 19:06:07 -0500

    Update about page

--- front-end/src/views/About.vue
+++ front-end/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="page mx-auto about">
+    <div class="intro bg-light p-2">
+      <h2>About</h2>
+    </div>
+    <div class="body px-2 mt-1">
+      <p class="text-muted"><i>The Book of Mormon: Parallel Reader</i> is a project by me (Jake Searle), a computer science undergrad from Brigham Young University.</p>
+
+      <h4>Inspiration:</h4>
+      <p>I served a full-time mission in Berlin, Germany and while I was there, I met members and friends alike who spoke all sorts of languages. I myself tried learning Chinese, in addition to German to help these people learn the Gospel, and saw what a difference it made for people to understand something in their native tongue.</p>
+      <p>While studying Chinese, I fell in love with the Chinese <a href="https://media.ldscdn.org/pdf/lds-scriptures/book-of-mormon/book-of-mormon-language-study-edition-34406-cmn_eng.pdf">language study edition</a> of the Book of Mormon and also <a href="https://thebible.org/gt/index">thebible.org</a>'s parallel Bible reader, from both of which I took some inspiration.</p>
+
+      <h4>Thanks:</h4>
+      <ul class="list-unstyled">
+        <li>+ To my parents and wife, for being my beta testers</li>
+        <li>+ To Christian Fingerle, who showed me the imporance of one's native language.</li>
+      </ul>
+    </div>
   </div>
 </template>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap');
+
+p, li {
+  font-family: 'Noto Serif', serif;
+}
+</style>
