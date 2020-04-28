44cf7f80b5e5f8601a08b936af3e33eac2946722
Jake Searle <jacobbsearle@gmail.com>
2020-04-27 19:06:40 -0500

    Contact me page created

---
+++ front-end/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="contact">
+    <h4>Suggestions?</h4>
+    <ul class="list-unstyled contact-list">
+      <li><a href="https://twitter.com/jacobbsearle"><img class="twitter" src="/images/logos/twitter.png" alt="twitter logo">Twitter: @jacobbsearle</a></li>
+      <li><a href="mailto:jacobbsearle@gmail.com?subject=feedback"><i class="material-icons text-dark">email</i>Email: jacobbsearle@gmail.com</a></li>
+      <li><a href="https://github.com/codingwizard37"><img class="github" src="/images/logos/github.png" alt="github logo">Github: codingwizard37</a></li>
+    </ul>
+    <h4>Feeling Generous?</h4>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
